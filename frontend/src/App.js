24f553f2ba4d9a5bd6d3a6c2c90c9adeb632692f
Uche <urch4gold@gmail.com>
2025-07-18 17:02:04 +0100

    modified app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Hello Uche! 👋</h1>
+      <h1>Hello Uche. Well done for the great work done! 👋</h1>
       <p>This is your first React UI containerized for learning!</p>
       <p>{count !== null ? `Visits recorded: ${count}` : 'Loading...'}</p>
     </div>
