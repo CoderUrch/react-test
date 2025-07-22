a8822964f1b2d97cb31439880be35d37b24d0272
Ubuntu <ubuntu@ip-172-31-0-35.eu-north-1.compute.internal>
2025-07-22 10:34:53 +0000

    Modified FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome! Cloud Engineer, Sylvia Abor - wife of Uche 👋</h1>
+      <h1>Welcome! Process  Engineer, Sylvia Abor 👋</h1>
       <p>This is your first React UI containerized for learning!</p>
       <p>{count !== null ? `Visits recorded: ${count}` : 'Loading...'}</p>
     </div>
